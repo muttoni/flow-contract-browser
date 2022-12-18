1488bb9618c15c9d3a5544dd1336f066cf84cb86
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2022-12-18 09:05:45 +0100

    added tables to account view0

--- src/lib/components/AccountView.svelte
+++ src/lib/components/AccountView.svelte
@@ -1,6 +1,7 @@
 <script>
-export let account = '';
-export let contracts = [];
+  export let account = '';
+  export let contracts = [];
+  import ContractTable from './ContractTable.svelte';
 </script>
 
 <article>
@@ -15,67 +16,58 @@ export let contracts = [];
     </h2>
   </hgroup>
   <a role="button" class="mb-1" target="_blank" rel="noreferrer" href="https://flowview.app/account/{account}">Inspect account on FlowView.app</a>
-  <div class="mb-1 grid">
-    <div>
-      <h4>Owned by {account}</h4>
-      <ul class="dep-list">
-        {#if contracts?.filter((s) => s.uuid.includes(account.substring(2)))?.length > 0}
-          {#each contracts?.filter((s) => s.uuid.includes(account.substring(2))) as c}
-            <li><a href="/{c.uuid}">{c.uuid}</a></li>
-          {/each}
-        {:else}
-          No contracts owned by {account}
-        {/if}
-      </ul>
-    </div>
-    <div>
-      <h4>Involving {account}</h4>
-      <ul class="dep-list">
-        {#if contracts?.filter((s) => !s.uuid.includes(account.substring(2)))?.length > 0}
-          {#each contracts?.filter((s) => !s.uuid.includes(account.substring(2))) as c}
-            <li><a href="/{c.uuid}">{c.uuid}</a></li>
-          {/each}
-        {:else}
-          No other contracts involving {account}
-        {/if}
-      </ul>
-    </div>
-    </div>
-  </article>
+  <div class="mb-1"> 
+    <h4>Owned by {account}</h4>
+    {#if contracts?.filter((s) => s.uuid.includes(account.substring(2)))?.length > 0}
+    <ContractTable contracts={contracts?.filter((s) => s.uuid.includes(account.substring(2)))}></ContractTable>
+    {:else}
+    No contracts owned by {account}
+    {/if}
+  </div>
+  <!-- <div>
+    <h4>Involving {account}</h4>
+    {#if contracts?.filter((s) => !s.uuid.includes(account.substring(2)))?.length > 0}
+    <ContractTable contracts={contracts?.filter((s) => !s.uuid.includes(account.substring(2)))}></ContractTable>
+    {:else}
+    No other contracts involving {account}
+    {/if}
+  </div>
+   -->
+</article>
+
+<style>
   
-  <style>
-    
-    h2 {
-      display:flex;
-      justify-content: start;
-      vertical-align: middle;
-      align-items: center;
-    }
-
-    .claimed-badge {
-      margin-left:1rem;
-      padding: 0.15rem 0.5rem;
-      border: 1px solid var(--green);
-      border-radius: 100px;
-      color: var(--green);
-      font-size: 0.7rem;
-      display:inline-block;
-    }
-
-    .success {
-      color: var(--green);
-    }
-
-    .dep-list {
-      font-size:0.8rem;
-    }
-
-    h2 {
-      margin-bottom: 0rem;
-    }
-
-    h4 {
-      margin-top: 40px;
-      margin-bottom: 20px;
-    }
-  </style>
+  h2 {
+    display:flex;
+    justify-content: start;
+    vertical-align: middle;
+    align-items: center;
+  }
+  
+  .claimed-badge {
+    margin-left:1rem;
+    padding: 0.15rem 0.5rem;
+    border: 1px solid var(--green);
+    border-radius: 100px;
+    color: var(--green);
+    font-size: 0.7rem;
+    display:inline-block;
+  }
+  
+  .success {
+    color: var(--green);
+  }
+  
+  .dep-list {
+    font-size:0.8rem;
+  }
+  
+  h2 {
+    margin-bottom: 0rem;
+  }
+  
+  h4 {
+    margin-top: 40px;
+    margin-bottom: 20px;
+  }
+</style>
