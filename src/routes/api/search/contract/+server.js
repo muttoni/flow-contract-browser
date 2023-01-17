5498f0a3ff4b433fb70b21dcf93aa31c2ebc1232
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2023-01-17 17:20:32 +0100

    enhancements

---
+++ src/routes/api/search/contract/+server.js
@@ -0,0 +1,25 @@
+import { json } from '@sveltejs/kit';
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ url }) {
+
+  const options = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_BACKEND_API_KEY}`
+    }
+  };
+  
+  let res = {};
+
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API_DOMAIN}/api/v1/contracts/search?keyword=${url.searchParams.get('query')}`, options)
+    res = await response.json()
+    res.success = true;
+  } catch(e) {
+    console.log(e)
+    res.success = false;
+  }
+
+  return json(res);
+};
