08cd6cab568d6712de9ce74ed87369746fc21b56
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2023-02-22 21:08:26 +0100

    updated seo

--- src/routes/[contract]/+page.svelte
+++ src/routes/[contract]/+page.svelte
@@ -5,7 +5,12 @@
 
 <svelte:head>
   <title>{data?.name} by {data?.address} | Flow Contract Browser</title>
-  <meta property="og:title" content="{data?.uuid} | Flow Contract Browser" />
+  <meta property="og:title" content="{data?.name} by {data?.address} | Flow Contract Browser" />
+  <meta name="twitter:title" content="{data?.name} by {data?.address} | Flow Contract Browser">
+  
+  <meta property="og:description" content="Inspect {data?.name}, deployed by {data?.address} - browse contracts and Cadence code on the Flow Blockchain using contractbrowser.com" />
+  <meta name="description" content="Inspect {data?.name}, deployed by {data?.address} - browse contracts and Cadence code on the Flow Blockchain using contractbrowser.com">
+  <meta name="twitter:description" content="Inspect {data?.name}, deployed by {data?.address} - browse contracts and Cadence code on the Flow Blockchain using contractbrowser.com">
 </svelte:head>
 
 {#if data && data.uuid}
