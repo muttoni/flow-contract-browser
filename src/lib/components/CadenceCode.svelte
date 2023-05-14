73799ea87bbc532e0a2f06cac89555ba7856d3a3
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2023-05-14 20:54:49 +0200

    onig

--- src/lib/components/CadenceCode.svelte
+++ src/lib/components/CadenceCode.svelte
@@ -1,22 +1,61 @@
-<script>
-import starry from '$lib/starrynight'
-import { onMount } from 'svelte';
-import CodeBlock from './CodeBlock.svelte';
-
-export let code = '';
-export let link = true;
-export let lineNumbers = true;
-
-$: htmlx = '';
-
-onMount(async () => {
-  htmlx = await starry.processCode(code, link);
-  // Use the processed HTML
-})
-
+<!-- <script>
+  import cadenceHighlighter from '$lib/cadencehighlighter'
+  import { onMount } from 'svelte';
+  import CodeBlock from './CodeBlock.svelte';
+  
+  export let code = '';
+  export let link = true;
+  export let lineNumbers = true;
+  
+  $: htmlx = '';
+  
+  onMount(async () => {
+    htmlx = await cadenceHighlighter.processCode(code, link);
+  })
 </script>
 
 
 <CodeBlock {lineNumbers} {code}>
   {@html htmlx}
+</CodeBlock> -->
+
+
+<script>
+  import { toHtml } from 'hast-util-to-html'
+  import { createStarryNight } from '@wooorm/starry-night'
+  import sourceCadence from '@wooorm/starry-night/lang/source.cadence'
+  import { detectAndAddAnchorLinksToAccounts } from '$lib/utils'
+  import CodeBlock from './CodeBlock.svelte';
+  
+  export let code = '';
+  export let link = true;
+  export let lineNumbers = true;
+  
+  $: htmlx = '';
+  
+  function myWasm(){
+    return new URL('/onig.wasm', window.location.href);
+  }
+  $: createStarryNight([sourceCadence], {getOnigurumaUrlFetch: myWasm})
+  .then((starryNight) => {
+    let tree = starryNight.highlight(code, 'source.cadence');
+    
+    let html = toHtml(tree)
+    
+    if(link) {
+      html = detectAndAddAnchorLinksToAccounts(html)
+    }
+    
+    htmlx = html;
+    
+  })
+  
+  
+  
+</script>
+
+
+<CodeBlock {lineNumbers} {code}>
+  {@html htmlx}
+  <!-- {code} -->
 </CodeBlock>
